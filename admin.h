016308245f176dd3d3033cb2d02b534fe16efb3a
maxyhwu <duffymax901225@gmail.com>
2022-11-24 02:18:31 +0800

    loading animation added

--- admin.h
+++ admin.h
@@ -2,39 +2,54 @@
 #include "./admin/fail.h"
 #include <string>
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include <iomanip>
 using namespace std;
 
 const int studentCnt = 50;
 int currentCnt = 0;
+float acceptCnt = 0;
 
 void success(){
-  if(currentCnt == studentCnt)
+  if(acceptCnt / currentCnt >= 0.8)
     doughnut();
   else
     failure();
 }
 
-string headerTable[80] = {
-  "apple",
-  "ball"
+// @Anthony
+
+pair <string, int> table[80] = {
+  make_pair("a", 0),
 };
 
-int ans[80] = {
-
-};
+//
 
 void loader(){
-  for (int i = 1; i <= 20; i++)
-  {
-    for (int j = 1; j <= i; j++)
-    {
-      cout << "#";
+  for (int i = 1; i <= 25; i++){
+    cout << "Loading";
+    for (int j = 1; j <= i; j++){
+      cout << ".";
     }
     cout << '\n';
+    std::this_thread::sleep_for(std::chrono::nanoseconds(50000000));
   }
   currentCnt++;
+  cout << "=================================================" << endl;
+  cout << endl;
+  cout << endl;
+  cout << "       Current Accepted Rate: " << setprecision(2) << 100 * (acceptCnt / studentCnt) << "%" << endl;
+  cout << endl;
+  cout << endl;
+  cout << "=================================================" << endl;
+  std::this_thread::sleep_for(std::chrono::seconds(3));
 }
 
 bool checker(){
+  /*
+  if legal
+    acCnt++
+  */
   return true;
 }
