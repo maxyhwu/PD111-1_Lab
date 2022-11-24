87d9f465b73ebb102560d957beeaf9829e3eb62e
Anthony Wu <91513821+AnthonyWu1121@users.noreply.github.com>
2022-11-24 12:25:25 +0800

    admin revise

--- admin.h
+++ admin.h
@@ -8,8 +8,8 @@
 #include <iomanip>
 using namespace std;
 
-const int studentCnt = 35;
-const int headerCnt = 80;
+const int studentCnt = 26;
+const int headerCnt = 26;
 int currentCnt = 0;
 float acceptCnt = 0;
 
@@ -22,24 +22,44 @@ void success(){
 
 // @Anthony
 
-pair <string, int> table[headerCnt] = {
-  make_pair("a", 0),
-};
-
-int result[headerCnt] = {
-  apple,
-
+pair <int, int> table[headerCnt] = {
+  make_pair(apple, 'a' + 'p' + 'p' + 'l' + 'e'),
+  make_pair(ball, 'b' + 'a' + 'l' + 'l'),
+  make_pair(car, 'c' + 'a' + 'r'),
+  make_pair(duck, 'd' + 'u' + 'c' + 'k'),
+  make_pair(egg, 'e' + 'g' + 'g'),
+  make_pair(fire, 'f' + 'i' + 'r' + 'e'),
+  make_pair(good, 'g' + 'o' + 'o' + 'd'),
+  make_pair(high, 'h' + 'i' + 'g' + 'h'),
+  make_pair(ink, 'i' + 'n' + 'k'),
+  make_pair(joy, 'j' + 'o' + 'y'),
+  make_pair(kid, 'k' + 'i' + 'd'),
+  make_pair(lion, 'l' + 'i' + 'o' + 'n'),
+  make_pair(may, 'm' + 'a' + 'y'),
+  make_pair(now, 'n' + 'o' + 'w'),
+  make_pair(opt, 'o' + 'p' + 't'),
+  make_pair(pro, 'p' + 'r' + 'o'),
+  make_pair(quiz, 'q' + 'u' + 'i' + 'z'),
+  make_pair(rest, 'r' + 'e' + 's' + 't'),
+  make_pair(star, 's' + 't' + 'a' + 'r'),
+  make_pair(team, 't' + 'e' + 'a' + 'm'),
+  make_pair(upon, 'u' + 'p' + 'o' + 'n'),
+  make_pair(vest, 'v' + 'e' + 's' + 't'),
+  make_pair(work, 'w' + 'o' + 'r' + 'k'),
+  make_pair(xray, 'x' + 'r' + 'a' + 'y'),
+  make_pair(yoyo, 'y' + 'o' + 'y' + 'o'),
+  make_pair(zoo, 'z' + 'o' + 'o')
 };
 
 //
 
 void loader(){
-  for (int i = 0; i <= 42; i++){
+  for (int i = 0; i <= 5; i++){
     cout << "Loading";
     for (int j = 0; j <= i; j++){
       cout << ".";
     }
-    std::this_thread::sleep_for(std::chrono::nanoseconds(30000000));
+    std::this_thread::sleep_for(std::chrono::nanoseconds(10000000));
     cout << endl;
   }
   currentCnt++;
@@ -50,11 +70,14 @@ void loader(){
   cout << endl;
   cout << endl;
   cout << "=================================================" << endl;
-  std::this_thread::sleep_for(std::chrono::seconds(3));
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  system("cls");
 }
 
 bool checker(){
-  if(result[currentCnt] == table[currentCnt].second)
+  if (currentCnt >= headerCnt)
+    return false;
+  if(table[currentCnt].first == table[currentCnt].second)
     acceptCnt++;
   return true;
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "allHeader.h"
 #include "admin.h"
 using namespace std;
 
